8616150579523c3ccd902396a982a34b7c88089a
Alok <alok22kr@gmail.com>
2022-03-25 08:25:39 +0530

    day 2

---
+++ src/app/mock-heros.ts
@@ -0,0 +1,11 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+{id : 11, name: 'Amitabh Bachhan'},
+{id: 12, name:'Rajpal Yadav'},
+{id: 13, name: 'Mithun Ch'},
+{id: 14, name: 'MS Dhoni'},
+{id: 15, name: 'Rohit Sharma'},
+{id: 16, name: 'Virat Kohli'},
+{id: 17, name: 'Rishabh Pant'}
+]
